c62ec4476f03c03321e770fb7f4b11b248439d10
Lee <pugglett@users.noreply.github.com>
2016-05-18 11:18:20 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+// var img1, img2;
+
+var smiley;
+
+function preload() {
+  // img1 = loadImage("images/sitting.png");
+  // img2 = loadImage("images/standing.png");
+  smiley = loadImage('images/smiley.png');
+}
+
+function setup() {
+  createCanvas(1000, 1000);
+  // img1 = loadImage("images/sitting.png");
+  // img2 = loadImage("images/standing.png");
+}
+
+function draw() {
+  function onClick() {
+    if (mouseX >= 500) {
+      image(img1, mouseX, mouseY);
+    } else {
+      image(img2, mouseX, mouseY);
+    }
+  }
+}
